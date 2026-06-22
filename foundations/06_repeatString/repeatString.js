73163aa1f189dbaf0834f70839c070f0145a22bc
Ali Reza <268299177+ali-reza-ejr@users.noreply.github.com>
2026-06-22 18:15:12 +0600

    Complete of JS Foundation exercise 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,7 +1,15 @@
-    const String = "hey"
-const repeatString = function() {
-    String.repeat(4)
+const repeatString = function(word, times) {
 
+// 1. Create your "holding container" variable here
+let finalResult = "";
+
+// 2. Write your loop here to repeat the action
+for (let i = 0; i < times; i++){
+finalResult += word;
+}
+
+// 3. Return your final result here
+return finalResult;
 };
 
 // Do not edit below this line
